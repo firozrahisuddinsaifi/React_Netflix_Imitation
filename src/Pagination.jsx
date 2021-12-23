d5a97911446c3b7bd7871b28919eca31a56ff840
firozrahisuddinsaifi <firozkhandelhi62@icloud.com>
2021-12-24 00:45:54 +0530

    deploy code

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -4,7 +4,7 @@ const Pagination=()=> {
     return (
         <>
         <div className="pagination">
-            <a href="/" className="anchoring">&laquo;</a>
+            {/* <a href="/" className="anchoring">&laquo;</a> */}
             <a href="/" className="anchoring">1</a>
             <a href="/" className="anchoring">2</a>
             <a href="/" className="anchoring">3</a>           
@@ -12,7 +12,7 @@ const Pagination=()=> {
             <a href="/" className="anchoring">5</a>
             <a href="/" className="anchoring">6</a>
             <a href="/" className="anchoring">7</a>
-            <a href="/" className="anchoring">&raquo;</a>
+            {/* <a href="/" className="anchoring">&raquo;</a> */}
             
         </div>
         <p className="copyright">All &copy;Copy Rights Are Reserved By The Netflix India </p>
